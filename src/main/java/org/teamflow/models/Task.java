7d9c154c1e88296c67b1bdb77414660b694b7d01
AndroidTheHuman <24109320@student.hhs.nl>
2025-04-10 11:56:45 +0200

    imporved task class

--- src/main/java/org/teamflow/models/Task.java
+++ src/main/java/org/teamflow/models/Task.java
@@ -1,28 +1,15 @@
 package org.teamflow.models;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Task
-{
-    private String title;
+public class Task {
+    private String content;
     private boolean isPinned;
-    private List<Message> messages;
 
-    public Task(String title)
-    {
-        this.title = title;
+    public Task(String content) {
+        this.content = content;
         this.isPinned = false;
-        this.messages = new ArrayList<>();
     }
 
-    public void pinTask()
-    {
-
-    }
-
-    public void addMessage(Message message)
-    {
-        this.messages.add(message);
+    public void pinTask() {
+        this.isPinned = true;
     }
 }
