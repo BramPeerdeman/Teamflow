18e30649c95a9ea85389ec9b0a2083880b825cb6
AndroidTheHuman <24109320@student.hhs.nl>
2025-04-10 11:37:18 +0200

    commit message class

--- src/main/java/org/teamflow/models/Message.java
+++ src/main/java/org/teamflow/models/Message.java
@@ -1,4 +1,31 @@
 package org.teamflow.models;
 
 public class Message {
+    private String inhoud;
+    private User afzender;
+
+    public Message (String inhoud, User afzender) {
+        this.inhoud = inhoud;
+        this.afzender = afzender;
+    }
+
+    public void setInhoud(String inhoud) {
+        this.inhoud = inhoud;
+    }
+
+    public void setAfzender(User afzender) {
+        this.afzender = afzender;
+    }
+
+    public String getInhoud() {
+        return inhoud;
+    }
+
+    public User getAfzender() {
+        return afzender;
+    }
+
+    public void toonBericht() {
+        System.out.printf("%s%n -%s", inhoud, afzender.getName());
+    }
 }
