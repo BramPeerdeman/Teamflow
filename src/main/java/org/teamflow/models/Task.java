c0b5ef8d4e59f5c0766c855b5cba9b3adeaa5d42
Bram <bram@fluet.dev>
2025-04-10 11:19:13 +0200

    task class base done

--- src/main/java/org/teamflow/models/Task.java
+++ src/main/java/org/teamflow/models/Task.java
@@ -1,4 +1,28 @@
 package org.teamflow.models;
 
-public class Task {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task
+{
+    private String title;
+    private boolean isPinned;
+    private List<Message> messages;
+
+    public Task(String title)
+    {
+        this.title = title;
+        this.isPinned = false;
+        this.messages = new ArrayList<>();
+    }
+
+    public void pinTask()
+    {
+
+    }
+
+    public void addMessage(Message message)
+    {
+        this.messages.add(message);
+    }
 }
