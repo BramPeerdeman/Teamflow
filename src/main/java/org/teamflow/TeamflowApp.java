3c61dfda9fba43217f6842fd7a34a873652232bd
AndroidTheHuman <24109320@student.hhs.nl>
2025-04-11 11:11:35 +0200

    cli and main class

--- src/main/java/org/teamflow/TeamflowApp.java
+++ src/main/java/org/teamflow/TeamflowApp.java
@@ -1,6 +1,13 @@
 package org.teamflow;
 
+import org.teamflow.cli.CLIHandler;
+
 public class TeamflowApp
 {
+    public static void main (String [] args)
 
+    {
+        CLIHandler cliHandler = new CLIHandler();
+        cliHandler.StartUp();
+    }
 }
--- src/main/java/org/teamflow/cli/CLIHandler.java
+++ src/main/java/org/teamflow/cli/CLIHandler.java
@@ -1,4 +1,29 @@
 package org.teamflow.cli;
 
-public class CLIHandler {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CLIHandler
+{
+    private ArrayList<String> asciiArt = new ArrayList<String>(Arrays.asList(
+            "",
+            "                                      ",
+            " |   |       |  | | __      ",
+            "   | |/  \/  | '_   \| |  | |/ _ \ \ /\ / /",
+            "   | |  / (| | | | | | |  | | | () \ V  V / ",
+            "   ||\|\,|| || |||   ||\__/ \/\/  ",
+            "",
+            "       -~= The scrum based chat-tool =~-              "
+    ));
+
+    public void StartUp() {
+        for (String s : asciiArt) {
+            System.out.println(s);
+            try {
+                Thread.sleep(56,250);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
